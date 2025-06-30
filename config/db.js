9fd3d6e9a41a9bc855697c48aed5921b20f08d9b
adityapohane <adityapohane9897@gmail.com>
2025-06-30 19:11:38 +0530

    changed index and make socket code more readable

--- config/db.js
+++ config/db.js
@@ -4,27 +4,34 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,       // e.g., 'localhost'
-  user: process.env.DB_USER,       // e.g., 'root'
+console.log(process.env.PORT)
+// Default configuration
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
-});
+};
 
+
+const pool = mysql.createPool(dbConfig);
+
+// Test the connection
 (async () => {
   try {
     const connection = await pool.getConnection();
-    console.log('✅ Connected to MySQL database');
+    console.log('Connected to MySQL database successfully.✅');
     connection.release();
   } catch (err) {
-    console.error('❌ Failed to connect to MySQL database:', err.message);
-    process.exit(1); // Optional: Exit the app if DB fails
+    console.error('Database connection failed:', err);
+
+    console.error('Make sure your MySQL server is running and accessible');
+    process.exit(1);
   }
 })();
-
 
 module.exports = pool;
