83a70ba6dff8d4e866046a7384ae47116941f956
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2025-07-01 01:57:40 +0530

    update cors

--- index.js
+++ index.js
@@ -28,13 +28,16 @@ const io = setupSocketIO(server)
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "*",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    maxAge: 86400,
+    origin: "*",
+   
   }),
 )
+
+
+//  credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     maxAge: 86400,
 
 app.use(
   fileUpload({
