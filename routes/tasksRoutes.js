475caccec113b85a7b599f15d0c61363f9f6901c
Aditya Pohane <adityapohane9897@gmail.com>
2025-07-03 01:41:18 +0530

    change in task routes

--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -2,31 +2,31 @@ const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/ai-task-ctrl');
 
-router.get('/patient/getTaskAnalytics', taskController.getTaskAnalytics);
-router.get('/patient/getTaskRecommendations', taskController.getTaskRecommendations);
-router.post('/patient/generateAutomatedTasks', taskController.generateAutomatedTasks);
+router.get('/getTaskAnalytics', taskController.getTaskAnalytics);
+router.get('/getTaskRecommendations', taskController.getTaskRecommendations);
+router.post('/generateAutomatedTasks', taskController.generateAutomatedTasks);
 
-router.post('/patient/startTaskTimer', taskController.startTaskTimer);
-router.post('/patient/stopTaskTimer', taskController.stopTaskTimer);
-router.get('/patient/getTaskTimeEntries', taskController.getTaskTimeEntries);
+router.post('/startTaskTimer', taskController.startTaskTimer);
+router.post('/stopTaskTimer', taskController.stopTaskTimer);
+router.get('/getTaskTimeEntries', taskController.getTaskTimeEntries);
 
-router.post('/patient/createWorkflowTemplate', taskController.createWorkflowTemplate);
-router.post('/patient/applyWorkflow', taskController.applyWorkflow);
-router.get('/patient/getWorkflowTemplates', taskController.getWorkflowTemplates);
+router.post('/createWorkflowTemplate', taskController.createWorkflowTemplate);
+router.post('/applyWorkflow', taskController.applyWorkflow);
+router.get('/getWorkflowTemplates', taskController.getWorkflowTemplates);
 
-router.get('/patient/getComplianceReport', taskController.getComplianceReport);
-router.get('/patient/getQualityMetrics', taskController.getQualityMetrics);
+router.get('/getComplianceReport', taskController.getComplianceReport);
+router.get('/getQualityMetrics', taskController.getQualityMetrics);
 
-// Get all tasks for a patient
-router.get('/patient/getAllPatientTasks', taskController.getAllPatientTasks);
+// Get all tasks
+router.get('/getAllTasks', taskController.getAllPatientTasks);
 
 // Create a new task
-router.post('/patient/addTask', taskController.addTask);
+router.post('/addTask', taskController.addTask);
 
 // Update an existing task
-router.put('/patient/updateTask/:id', taskController.updateTask);
+router.put('/updateTask/:id', taskController.updateTask);
 
 // Delete a task
-router.delete('/patient/deleteTask/:id', taskController.deleteTask);
+router.delete('/deleteTask/:id', taskController.deleteTask);
 
 module.exports = router;
