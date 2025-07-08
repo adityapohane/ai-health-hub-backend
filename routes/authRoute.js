415e64c57f933d371b418490397265ec953e5d61
Aditya Pohane <adityapohane9897@gmail.com>
2025-07-08 16:50:17 +0530

    added routes for resetting password

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,14 @@
 const express = require("express")
-const { registerCtrl, loginCtrl, changePasswordCtrl } = require("../controllers/authCtrl")
+const { registerCtrl, loginCtrl, changePasswordCtrl } = require("../controllers/authCtrl");
+const { resetPasswordCtrl, resetPasswordTokenCtrl } = require("../controllers/resetPasswordCtrl");
 const { verifyToken } = require("../middleware/auth")
 const router = express.Router()
 
 
 router.post("/login", loginCtrl)
 router.post("/signup", registerCtrl)
+router.post("/reset-password", resetPasswordCtrl)
+router.post("/reset-password-token", resetPasswordTokenCtrl)
 router.post("/change-password",verifyToken, changePasswordCtrl)
 
 
