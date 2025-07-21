7cf4dcb66811d7fd58354e1bd37ad9163e18f254
Aditya Pohane <adityapohane9897@gmail.com>
2025-07-21 21:19:43 +0530

    update

--- services/appointment/appointmentRoutes.js
+++ services/appointment/appointmentRoutes.js
@@ -11,7 +11,7 @@ const router = express.Router();
 router.post("/create", createAppointment);
 router.get("/provider/:providerId", getAppointmentsByProviderId);
 router.get("/upcoming/:providerId", upcomingAppointments);
-router.get("/update-status/:providerId", updateAppointmentStatus);
+router.post("/update-status/:providerId", updateAppointmentStatus);
 
 
 module.exports = router;
