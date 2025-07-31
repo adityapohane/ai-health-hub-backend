d146720fd0cb46b079fe89d5acac0a6fee3d0b51
Aditya Pohane <adityapohane9897@gmail.com>
2025-07-31 18:10:41 +0530

    register patient with intake

--- services/intake/intakeForm.js
+++ services/intake/intakeForm.js
@@ -1,4 +1,6 @@
 const mailSender = require("../../utils/mailSender");
+const connection = require("../../config/db");
+const bcrypt = require("bcryptjs");
 
 const sendIntake = async (req, res) => {
     try {
@@ -30,4 +32,157 @@ const sendIntake = async (req, res) => {
     }
 };
 
-module.exports = sendIntake
+const registerPatient = async (req, res) => {
+        try {
+          const {
+            firstName,
+            middleName,
+            lastName,
+            email,
+            phone,
+            gender,
+            status,
+            birthDate,
+            lastVisit,
+            emergencyContact,
+            ethnicity,
+            insurance,
+            addressLine1,
+            addressLine2,
+            city,
+            state,
+            country,
+            zipCode,
+            organizationId,
+            practiceId,
+            patientService
+          } = req.body;
+          let password = `${firstName}@hub`;
+      
+          const [rows] = await connection.query(
+            'SELECT 1 FROM users WHERE username = ? LIMIT 1',
+            [email]
+          );
+          if (rows.length > 0) {
+      
+            return res.status(401).send({ success: false, message: 'email already\u00a0exists' });
+          }
+      
+          const hashedPassword = await bcrypt.hash(password, 10);
+          const insertQuery =
+            "INSERT INTO users (username, password,fk_roleid,created_user_id) VALUES (?, ?,7,?)";
+          const { user_id, username } = req.user;
+          const userValue = [email, hashedPassword, user_id];
+          const [result] = await connection.query(insertQuery, userValue);
+          const insertedId = result.insertId;
+      
+          const sql1 = `
+      INSERT INTO user_profiles (
+        firstname, middlename, lastname, dob, work_email, phone,
+        gender, ethnicity, last_visit, emergency_contact,
+        address_line, address_line_2, city, state, country, zip, service_type, status, fk_userid
+      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+      `;  
+      const values1 = [
+        firstName,        // 1. firstname
+        middleName,       // 2. middlename
+        lastName,         // 3. lastname
+        birthDate,        // 4. dob
+        email,            // 5. work_email
+        phone,            // 6. phone
+        gender,           // 7. gender
+        ethnicity,        // 8. ethnicity
+        lastVisit,        // 9. last_visit
+        emergencyContact, // 10. emergency_contact
+        addressLine1,     // 11. address_line
+        addressLine2,     // 12. address_line_2
+        city,             // 13. city
+        state,            // 14. state
+        country,          // 15. country
+        zipCode,          // 16. zip
+        JSON.stringify(patientService), // 17. service_type
+        status,           // 18. status
+        insertedId        // 19. fk_userid
+      ];
+      
+          const [userResult] = await connection.query(sql1, values1);
+      
+          const sql3 = `INSERT INTO patient_insurances (
+            insurance_policy_number,
+            insurance_group_number,
+            insurance_company,
+            insurance_plan,
+            insurance_relationship,
+            insurance_expiry,
+            insurance_type,
+            effective_date,
+            insured_name,
+            insured_gender,
+            insured_dob,
+            insured_address,
+            insured_phone,
+            fk_userid
+          ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`;
+          insurance?.map(async (insurance) => {
+            const values3 = [
+              insurance.policyNumber,
+              insurance.groupNumber,
+              insurance.company,
+              insurance.plan,
+              insurance.relationship,
+              insurance.expirationDate,
+              insurance.type,
+              insurance.effectiveDate,
+              insurance.insuredName,
+              insurance.insuredGender,
+              insurance.insuredDOB,
+              insurance.insuredAddress,
+              insurance.insuredPhone,
+              insertedId,
+            ];
+            const [insuranceResult] = await connection.query(sql3, values3);
+          });
+      
+          const sql7 = `INSERT INTO users_mappings (
+        organizations_id,
+        practice_id,
+        user_id,
+        fk_role_id,
+        fk_physician_id,
+        fk_nurse_id
+      ) VALUES (?, ?, ?, ?, ?, ?);`;
+      
+          const values7 = [
+            organizationId ? organizationId : 0,
+            practiceId ? practiceId : 0,
+            insertedId,
+            7,
+            user_id ? user_id : 0,
+            nurseId ? nurseId : 0,
+          ];
+          const [mappingResult] = await connection.query(sql7, values7);
+      
+          // cpt billing for rpm
+          patientService?.includes(1) && await connection.query(
+            `INSERT INTO cpt_billing (patient_id, cpt_code_id) VALUES (?, ?);`,
+            [insertedId, 4]
+          );
+      
+          await logAudit(req, 'CREATE', 'PATIENT', insertedId, `Patient created with patientId: ${insertedId} - ${firstName} ${lastName}`);
+          return res.status(200).json({
+            success: true,
+            message: "User registered successfully",
+          });
+        } catch (error) {
+          console.error("Error creating patient:", error);
+          res
+            .status(500)
+            .json({ success: false, message: "Error in create patient API" });
+        }
+      };
+
+
+module.exports = {
+    sendIntake,
+    registerPatient
+}
--- services/intake/intakeRoute.js
+++ services/intake/intakeRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const sendIntake = require("./intakeForm"); // adjust path as needed
-
+const {sendIntake,registerPatient} = require("./intakeForm"); // adjust path as needed
 router.post("/send", sendIntake);
+router.post("/register/patient", registerPatient);
 
 module.exports = router;
