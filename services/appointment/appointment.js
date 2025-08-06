1af4526f8a7a0a42cbb1dfd9fae083dd008c247f
Aditya Pohane <adityapohane9897@gmail.com>
2025-08-06 09:28:25 -0400

    templates in appointment changes

--- services/appointment/appointment.js
+++ services/appointment/appointment.js
@@ -13,11 +13,11 @@ exports.createAppointment = async (req, res) => {
       hasBilling,
       providerId,
       locationId,
+      template_id,
       reason,
     } = req.body;
 
-    console.log(req.body)
-    if (!providerId || !patient?.id || !date || !duration) {
+    if (!providerId || !patient?.id || !date || !duration || !template_id) {
       return res.status(400).json({ success: false, message: "Missing required fields" });
     }
 
@@ -63,12 +63,12 @@ exports.createAppointment = async (req, res) => {
       INSERT INTO appointment (
         id, patient_id, patient_name, patient_phone, patient_email,
         date, duration, type, status, has_billing,
-        provider_id, location_id, reason
-      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        provider_id, location_id, reason, template_id
+      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     `;
 
     const values = [
-      id,
+      id || null,
       patient.id,
       patient.name,
       patient.phone,
@@ -81,6 +81,7 @@ exports.createAppointment = async (req, res) => {
       providerId,
       locationId,
       reason,
+      template_id,
     ];
 
     await db.execute(insertQuery, values);
@@ -108,7 +109,7 @@ exports.getAppointmentsByProviderId = async (req, res) => {
       [providerId]
     );
 
-    const transformed = appointments.map((row) => {
+    const transformed = await Promise.all(appointments.map(async (row) => {
       const utcDate = new Date(row.date); // Date from MySQL (in UTC)
 
       // ✅ Manually convert to IST by adding 5 hours 30 minutes
@@ -126,7 +127,13 @@ exports.getAppointmentsByProviderId = async (req, res) => {
       const ss = pad(istDate.getSeconds());
 
       const formattedIST = `${yyyy}-${mm}-${dd}T${hh}:${min}:${ss}+05:30`;
-
+      if(row.template_id){
+        const [template] = await db.execute(
+          "SELECT template_id,encounter_name, encounter_type, visit_type, is_default, is_active, soap_structure, billing_codes, created_by,created  FROM providers_encounter_template WHERE template_id = ?",
+          [row.template_id]
+        );
+        row.template = template.length > 0 ? template[0] : null;
+      }
       return {
         id: row.id,
         patient: {
@@ -143,8 +150,9 @@ exports.getAppointmentsByProviderId = async (req, res) => {
         providerId: row.provider_id,
         locationId: row.location_id,
         reason: row.reason,
+        template: row.template || null,
       };
-    });
+    }));
 
     res.status(200).json({ success: true, data: transformed });
   } catch (err) {
